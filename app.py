40dc43631e4ef86f12d9027b8dafa0f20eea8ce1
omarraf <omarrafiqq@gmail.com>
2025-04-18 00:11:47 -0700

    fetch and display first 10 NBA players

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+from nba_api.stats.static import players
+import pandas as pd
+
 
 
 st.title("NBA Player Comparison Dashboard")
+
+player_list = players.get_players()
+df = pd.DataFrame(player_list)
+st.write("first 10 nba players in the database:")
+st.table(df[['full_name']].head(10))
