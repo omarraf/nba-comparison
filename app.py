dfb37b98a4936f00adb519ccb1f1e25bd7691df1
omarraf <omarrafiqq@gmail.com>
2025-04-18 14:21:52 -0700

    add player dropdown

--- app.py
+++ app.py
@@ -10,3 +10,9 @@ player_list = players.get_players()
 df = pd.DataFrame(player_list)
 st.write("first 10 nba players in the database:")
 st.table(df[['full_name']].head(10))
+
+player_names = df['full_name'].tolist()
+selected_player = st.selectbox("Select an NBA player:", player_names)
+player_id = df[df['full_name'] == selected_player]['id'].values[0]
+st.write("selected player: {selected_player} (ID: {player_id})")
+
